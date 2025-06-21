74d935145e68ac7b1be20f7d450f61dff0245dc5
mittal <shikhar.mittal4@gmail.com>
2025-06-21 10:45:14 +0100

    debugging

--- src/echo21/echofuncs.py
+++ src/echo21/echofuncs.py
@@ -153,8 +153,8 @@ class funcs():
 
             fcoll_slice = f_coll[i_mdm, i_sigma, :, :]
 
-            interpolator_2d = RegularGridInterpolator((zvals, halomass_vals),fcoll_slice,bounds_error=False,fill_value=np.nan)
-
+            #interpolator_2d = RegularGridInterpolator((zvals, halomass_vals),fcoll_slice,bounds_error=False,fill_value=np.nan)
+            
             #mdm_sigma_interp = RegularGridInterpolator(
             #(mdmeff_vals, sigma0_vals),
             #f_coll,  # 4D array
@@ -164,7 +164,7 @@ class funcs():
             # Get HMF slice at desired mdm_eff and sigma0: shape (Nz, Nmass)
             #fcoll_z_mass = mdm_sigma_interp((self.mx_gev, 1e4*self.sigma0)).reshape(len(zvals), len(halomass_vals))
             
-            self.rbs = interpolator_2d #RectBivariateSpline(zvals, halomass_vals, fcoll_z_mass)
+            self.rbs = RectBivariateSpline(zvals, halomass_vals, fcoll_slice)
 
             self._f_coll = self._f_coll_idm
             self._igm_eqns = self._igm_eqns_idm
@@ -497,7 +497,7 @@ class funcs():
         if np.any(valid):
             Z_valid = Z[valid]
             mmin = self.m_min(Z_valid) / self.h100
-            results[valid] = self.rbs((Z_valid - 1,mmin))
+            results[valid] = self.rbs.ev(Z_valid - 1,mmin)
             
         return results[0] if scalar_input else results
     
