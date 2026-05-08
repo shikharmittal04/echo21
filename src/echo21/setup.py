c2dd2b4b0f531cfe7e8ae2af8e1def5d1f84c0cf
mittal <shikhar.mittal4@gmail.com>
2026-05-08 20:57:33 +0100

    Updating docs

--- src/echo21/setup.py
+++ src/echo21/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='echo21',
-version='1.1.2',
+version='4.1.0',
 description='Exploring the Cosmos with Hydrogen Observation',
 url='https://github.com/shikharmittal04/echo21/',
 author='Shikhar Mittal',
